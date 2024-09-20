07e59938e7783784fbc386f7fe07f4a98cb1402f
albad <albad@192.168.1.23>
2024-09-20 19:14:03 +0200

    Exercise6

---
+++ src/exercise6/Exercise6.java
@@ -0,0 +1,14 @@
+package exercise6;
+
+public class Exercise6 {
+
+		public static void main(String[] args) {
+			System.out.println("Name: Alba.");
+			System.out.println("Address: Calle Palmillas nº 26.");
+			System.out.println("");
+			System.out.println("Telephone number: 634 03 89 55.");// TODO Auto-generated method stub
+
+		}
+
+	}
+
